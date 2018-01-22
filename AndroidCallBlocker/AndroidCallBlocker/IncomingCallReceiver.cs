1e620826d69d499148e145178faed527b87f5752
Doug Lenox <lenoxdj@miamioh.edu>
2018-01-21 18:16:56 -0800

    Persist setting to block calls

--- AndroidCallBlocker/AndroidCallBlocker/IncomingCallReceiver.cs
+++ AndroidCallBlocker/AndroidCallBlocker/IncomingCallReceiver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.Content;
+using Android.Runtime;
 using Android.Telephony;
 
 namespace AndroidCallBlocker
@@ -10,12 +11,25 @@ namespace AndroidCallBlocker
         public override void OnReceive(Context context, Intent intent)
         {
             var tm = (TelephonyManager)context.GetSystemService(Context.TelephonyService);
-
             String incomingNumber = intent.Extras.GetString("incoming_number");
 
-            if((incomingNumber != null) && IsPhoneNumberSimilar(tm.Line1Number, incomingNumber))
+            if (MainActivity.BlockSimilarNumbers && (incomingNumber != null) && IsPhoneNumberSimilar(tm.Line1Number, incomingNumber))
             {
+                IntPtr TelephonyManager_getITelephony = JNIEnv.GetMethodID(
+                   tm.Class.Handle,
+                   "getITelephony",
+                   "()Lcom/android/internal/telephony/ITelephony;");
 
+                IntPtr telephony = JNIEnv.CallObjectMethod(tm.Handle, TelephonyManager_getITelephony);
+                IntPtr ITelephony_class = JNIEnv.GetObjectClass(telephony);
+                IntPtr ITelephony_endCall = JNIEnv.GetMethodID(
+                        ITelephony_class,
+                        "endCall",
+                        "()Z");
+
+                JNIEnv.CallBooleanMethod(telephony, ITelephony_endCall);
+                JNIEnv.DeleteLocalRef(telephony);
+                JNIEnv.DeleteLocalRef(ITelephony_class);
             }
         }
 
--- AndroidCallBlocker/AndroidCallBlocker/MainActivity.cs
+++ AndroidCallBlocker/AndroidCallBlocker/MainActivity.cs
@@ -1,13 +1,16 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace AndroidCallBlocker
 {
     [Activity(Label = "AndroidCallBlocker", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        public bool BlockSimilarNumbers;
+        public static bool BlockSimilarNumbers;
+        private const string AppName = "AndroidCallBlocker";
+        private const string BlockSimilarNumbersKeyName = "BlockSimilarNumbers";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,9 +21,19 @@ namespace AndroidCallBlocker
 
             CheckBox similarNumberChk = FindViewById<CheckBox>(Resource.Id.blockSimilarNumbersChk);
 
+            // Get initial state of similarNumberChk
+            similarNumberChk.Checked = Application.Context.GetSharedPreferences(AppName, FileCreationMode.Private)
+                .GetBoolean(BlockSimilarNumbersKeyName, false);
+
+            BlockSimilarNumbers = similarNumberChk.Checked;
+
+            // Handle changes to checked state
             similarNumberChk.Click += (o, e) =>
             {
                 BlockSimilarNumbers = similarNumberChk.Checked;
+                var prefEditor = Application.Context.GetSharedPreferences(AppName, FileCreationMode.Private).Edit();
+                prefEditor.PutBoolean(BlockSimilarNumbersKeyName, BlockSimilarNumbers);
+                prefEditor.Commit();
             };
         }
     }
