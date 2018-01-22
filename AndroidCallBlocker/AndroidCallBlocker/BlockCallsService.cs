4d3f8ecbde15080211ea3dcd90fd358ebd04b942
Doug Lenox <lenoxdj@miamioh.edu>
2018-01-21 19:08:52 -0800

    Add basic service skeleton

---
+++ AndroidCallBlocker/AndroidCallBlocker/BlockCallsService.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace AndroidCallBlocker
+{
+    [Service]
+    public class BlockCallsService : Service
+    {
+        private bool isStarted = false;
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            isStarted = true;
+
+            return StartCommandResult.NotSticky;
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnDestroy()
+        {
+            isStarted = false;
+        }
+    }
+}
